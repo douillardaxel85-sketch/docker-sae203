43f2c164102d853f07cf3c450e97975c9699390e
Armand Dupont <armand.dupont@etu.univ-lehafre.fr>
2026-04-23 01:11:34 +0200

    ajout de ma v1 de l'ihm de l'app

---
+++ v1ihm/ClasseTest.java
@@ -0,0 +1,7 @@
+public class ClasseTest
+{
+	public ClasseTest(String nomFichier, String idMachine, int port)
+	{
+		System.out.println("envoi du fichier "+ nomFichier+ " vers la machine "+idMachine+" avec le port "+port);
+	}
+}
---
+++ v1ihm/FrameIhm.java
@@ -0,0 +1,19 @@
+import java.awt.event.*;
+import javax.swing.*;
+public class FrameIhm extends JFrame
+{
+	private JPanel panelApp;
+	public FrameIhm()
+	{
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setTitle("envoi de fichier");
+		this.setSize(500,300);
+		panelApp = new PanelIhm();
+		this.add(panelApp);
+		this.setVisible(true);
+	}
+	public static void main(String[] a)
+	{
+		new FrameIhm();
+	}
+}
