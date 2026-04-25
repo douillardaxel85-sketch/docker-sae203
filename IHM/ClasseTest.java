d5ae980ecef48840eb606e0385e557024f90e056
PalCkam <palckam07@gmail.com>
2026-04-25 16:07:15 +0200

    Nettoyage des fonction test + Lanceur avec menu

--- IHM/ClasseTest.java
+++
@@ -1,7 +0,0 @@
-public class ClasseTest
-{
-	public ClasseTest(String nomFichier, String idMachine, int port)
-	{
-		System.out.println("envoi du fichier "+ nomFichier+ " vers la machine "+idMachine+" avec le port "+port);
-	}
-}
